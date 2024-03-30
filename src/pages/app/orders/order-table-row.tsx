90a330827ac21f50510ca4d662167f5600adc1d9
Felipe Makoto Gushiken Yui <felipe_m87@yahoo.com.br>
2024-03-30 23:53:13 +1030

    order filter

---
+++ src/pages/app/orders/order-table-row.tsx
@@ -0,0 +1,41 @@
+import { ArrowRight, Search, X } from 'lucide-react'
+
+import { Button } from '@/components/ui/button'
+import { TableCell, TableRow } from '@/components/ui/table'
+
+export function OrderTableRow() {
+  return (
+    <TableRow>
+      <TableCell>
+        <Button variant="outline" size="xs">
+          <Search className="h-4 w-4" />
+          <span className="sr-only">Order Details</span>
+        </Button>
+      </TableCell>
+      <TableCell className="font-mono text-xs font-medium">
+        1234213dsfg
+      </TableCell>
+      <TableCell className="text-muted-foreground">15 minutes ago</TableCell>
+      <TableCell>
+        <div className="flex items-center gap-2">
+          <span className="h-2 w-2 rounded-full bg-slate-400" />
+          <span className="font-medium text-muted-foreground">Pending</span>
+        </div>
+      </TableCell>
+      <TableCell className="font-medium">Felipe Yui</TableCell>
+      <TableCell className="font-medium">AU$ 200.00</TableCell>
+      <TableCell>
+        <Button variant="outline" size="xs">
+          <ArrowRight className="mr-2 h-3 w-3" />
+          Approve
+        </Button>
+      </TableCell>
+      <TableCell>
+        <Button variant="ghost" size="xs">
+          <X className="mr-2 h-3 w-3" />
+          Cancel
+        </Button>
+      </TableCell>
+    </TableRow>
+  )
+}
--- src/pages/app/orders/orders.tsx
+++ src/pages/app/orders/orders.tsx
@@ -1,29 +1,26 @@
-import { ArrowRight, Search, X } from 'lucide-react'
 import { Helmet } from 'react-helmet-async'
 
-import { Button } from '@/components/ui/button'
-import { Input } from '@/components/ui/input'
 import {
   Table,
   TableBody,
-  TableCell,
   TableHead,
   TableHeader,
   TableRow,
 } from '@/components/ui/table'
 
+import { OrderTableFilters } from './order-table-filters'
+import { OrderTableRow } from './order-table-row'
+
 export function Orders() {
   return (
     <>
       <Helmet title="Orders" />
+
       <div className="flex flex-col gap-4">
-        <h1 className="text-3xl font-bold tracking-tight">Orders</h1>
+        <h1 className="text-3xl font-bold tracking-tight">Pedidos</h1>
       </div>
-      <div className="space-y-2.5 ">
-        <form className="flex items-center gap-2">
-          <span className="text-sm font-semibold">Filters</span>
-          <Input placeholder="Client name" className="h-8 w-[320px]" />
-        </form>
+      <div className="mt-2 space-y-2.5">
+        <OrderTableFilters />
         <div className="rounded-md border">
           <Table>
             <TableHeader>
@@ -40,44 +37,7 @@ export function Orders() {
             </TableHeader>
             <TableBody>
               {Array.from({ length: 10 }).map((_, index) => {
-                return (
-                  <TableRow key={index}>
-                    <TableCell>
-                      <Button variant="outline" size="xs">
-                        <Search className="h-4 w-4" />
-                        <span className="sr-only">Order Details</span>
-                      </Button>
-                    </TableCell>
-                    <TableCell className="font-mono text-xs font-medium">
-                      1234213dsfg
-                    </TableCell>
-                    <TableCell className="text-muted-foreground">
-                      15 minutes ago
-                    </TableCell>
-                    <TableCell>
-                      <div className="flex items-center gap-2">
-                        <span className="h-2 w-2 rounded-full bg-slate-400" />
-                        <span className="font-medium text-muted-foreground">
-                          Pending
-                        </span>
-                      </div>
-                    </TableCell>
-                    <TableCell className="font-medium">Felipe Yui</TableCell>
-                    <TableCell className="font-medium">AU$ 200.00</TableCell>
-                    <TableCell>
-                      <Button variant="outline" size="xs">
-                        <ArrowRight className="mr-2 h-3 w-3" />
-                        Approve
-                      </Button>
-                    </TableCell>
-                    <TableCell>
-                      <Button variant="ghost" size="xs">
-                        <X className="mr-2 h-3 w-3" />
-                        Cancel
-                      </Button>
-                    </TableCell>
-                  </TableRow>
-                )
+                return <OrderTableRow key={index} />
               })}
             </TableBody>
           </Table>
