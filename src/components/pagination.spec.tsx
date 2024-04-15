cd798cf7699f0665143c582f4a9b784ed6c9574e
Felipe Makoto Gushiken Yui <felipe_m87@yahoo.com.br>
2024-04-15 22:59:37 +0930

    using spies

---
+++ src/components/pagination.spec.tsx
@@ -0,0 +1,41 @@
+import { render } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+
+import { Pagination } from './pagination'
+
+const onChangePageCallback = vi.fn()
+
+describe('Pagination', () => {
+  it('should display the right amount of pages and results', () => {
+    const wrapper = render(
+      <Pagination
+        pageIndex={0}
+        totalCount={200}
+        perPage={10}
+        onPageChange={() => {}}
+      />,
+    )
+
+    expect(wrapper.getByText('Page 1 of 20')).toBeInTheDocument()
+    expect(wrapper.getByText('Total of 200 item(s)')).toBeInTheDocument()
+  })
+  it('should be able to navigate to the next page', async () => {
+    const user = userEvent.setup()
+    const wrapper = render(
+      <Pagination
+        pageIndex={0}
+        totalCount={200}
+        perPage={10}
+        onPageChange={onChangePageCallback}
+      />,
+    )
+
+    const nextPageButton = wrapper.getByRole('button', {
+      name: 'Next Page',
+    })
+
+    await user.click(nextPageButton)
+
+    expect(onChangePageCallback).toHaveBeenCalledWith(1)
+  })
+})
